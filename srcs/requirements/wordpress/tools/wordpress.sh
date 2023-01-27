6ddd541b208552650890822644885f1148ce2a0f
Souhail Ismaili alaoui <sismaili@e1r4p7.1337.ma>
2023-01-27 01:49:30 +0100

    creating wordpress script

---
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+
+mv wp-cli.phar /usr/bin/wp
+
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html
+
+cd /var/www/html
+
+wp core download --allow-root
+
+cp wp-config-sample.php wp-config.php
+
+wp config set DB_NAME $MYSQL_DATABASE --allow-root
+wp config set DB_USER $MYSQL_USER --allow-root
+wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root
+wp config set DB_HOST $MYSQL_HOST --allow-root
+
+wp core install --url=$DOMAIN_NAME --title="WordPress Site" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
+
+wp user create $USER $USER_EMAIL --user_pass=$USER_PASSWORD role='author' --allow-root
+
+exec "$@"
