54f0f145bc002f66f34c9bed3275f3799ecb0fac
Souhail37 <i.alaoui.souhail@gmail.com>
2023-02-23 01:06:33 +0100

    modifying mariadb script

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -9,14 +9,12 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
 
-echo "Database: $MYSQL_DATABASE, User: $MYSQL_USER, Password: $MYSQL_PASSWORD" > test.txt
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';"
 
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';"
-
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+mysql -u root -e "FLUSH PRIVILEGES;"
 
 kill `cat /var/run/mysqld/mysqld.pid`
